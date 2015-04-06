1d1545e263b5c34d7fda07bda780bc1c09070b3b
Alex Clemmer <clemmer.alexander@gmail.com>
2015-04-06 15:08:29 -0700

    Add functioning CentOS master node config script

--- ubuntu-vm-mesosphere-cluster/configure_master.sh
+++ ubuntu-vm-mesosphere-cluster/configure_master.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-# Add mesosphere repository to hosts, so we can install it. Basically involves:
-#   1. Downloading the proj key from Ubuntu servers
-#   2. Splicing the correct URL for this specific Ubuntu version
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-CODENAME=$(lsb_release -cs)
-echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+# ---                                     ---
+# --- MESOSPHERE CONFIG SCRIPT FOR CENTOS ---
+# ---                                     ---
 
-# Install Mesosphere
-sudo apt-get -y update
-sudo apt-get install mesosphere
+# Add the repository
+rpm -Uvh http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
 
-# Set up ZK connection info
-# TODO: get a random number of arguments, put them into a single config string
-#       and pipe them to the /etc/mesos/zk file!
-sudo echo "zk://192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181/mesos" > /etc/mesos/zk
+# Install Mesos and ZK
+yum -y install mesos marathon
+yum -y install mesosphere-zookeeper
 
-# Master ZK configuration
-sudo echo $MASTER_ID > /etc/zookeeper/conf/myid
+# Configure ZK
+ID=1
+ID=`expr "$1" + "$ID"`
+echo $ID | tee /var/lib/zookeeper/myid
+
+echo | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "server.1=${2}:2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "server.1=${3}:2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "server.1=${4}:2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+
+# Start ZK
+systemctl start zookeeper
+
+# Mesos & Marathon configuration
+echo "zk://${2}:2181,${3}:2181,${4}:2181/mesos" | tee /etc/mesos/zk
+echo 2 | tee /etc/mesos-master/quorum
+
+# Disable mesos-slave service
+systemctl stop mesos-slave.service
+systemctl disable mesos-slave.service
+
+# Restart Mesos/Marathon services to bring them up at roughly the same time
+service mesos-master restart
+service marathon restart
