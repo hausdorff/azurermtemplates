3931031ca87eac367f90c9be3b3f908693f289e2
Alex Clemmer <clemmer.alexander@gmail.com>
2015-04-07 16:49:13 -0700

    Add slave node spinup logic

--- ubuntu-vm-mesosphere-cluster/configure_slave.sh
+++ ubuntu-vm-mesosphere-cluster/configure_slave.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# Add mesosphere repository to hosts, so we can install it. Basically involves:
-#   1. Downloading the proj key from Ubuntu servers
-#   2. Splicing the correct URL for this specific Ubuntu version
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
-DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-CODENAME=$(lsb_release -cs)
-echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+# Add the repository
+echo "---                                         ---"
+echo "--- Installing Mesos and ZooKeeper packages ---"
+echo "---                                         ---"
+rpm -Uvh http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
 
 # Install Mesos
-sudo apt-get -y update
-sudo apt-get install mesos
+yum -y install mesos
 
-# Set up ZK connection info
-# TODO: get a random number of arguments, put them into a single config string
-#       and pipe them to the /etc/mesos/zk file!
-sudo echo "zk://192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181/mesos" > /etc/mesos/zk
+# Configure ZK
+echo "---                       ---"
+echo "--- Configuring ZooKeeper ---"
+echo "---                       ---"
+echo "zk://${2}:2181,${3}:2181,${4}:2181/mesos" | tee /etc/mesos/zk
 
+# Disable the Mesos master services
+echo "---                                ---"
+echo "--- Stopping Mesos master services ---"
+echo "---                                ---"
+systemctl stop mesos-master.service
+systemctl disable mesos-master.service
+
+# Start the Mesos slave services
+echo "---                               ---"
+echo "--- Starting Mesos slave services ---"
+echo "---                               ---"
+service mesos-slave restart
